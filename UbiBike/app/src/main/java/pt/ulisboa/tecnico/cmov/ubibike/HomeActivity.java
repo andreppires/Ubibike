153348a219e7a1a37db66c7f237a8b0e7338d24d
André Pires <andre.pinheiro.pires@gmail.com>
2016-04-07 00:56:12 +0100

    interação entre algumas das activities sing in addicionado (é so colocar um e-mail - algo com arroba no meio ao calhas) e le passa para o home. estoira no home ao carregar no profile (dunno!)

--- UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/HomeActivity.java
+++ UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/HomeActivity.java
@@ -1,7 +1,9 @@
 package pt.ulisboa.tecnico.cmov.ubibike;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,4 +12,21 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    public void myProfileActivity(View view) {
+        Intent intent = new Intent(this, MyProfile.class);
+        startActivity(intent);
+    }
+    public void bikesActivity(View view) {
+        Intent intent = new Intent(this, BikesNeirby.class);
+        startActivity(intent);
+    }
+    public void friendsActivity(View view) {
+        Intent intent = new Intent(this, FriendsActivity.class);
+        startActivity(intent);
+    }
+    //public void stationActivity(View view) { //TODO quando se tiver a activity das estações - nome deve ser StationActivity
+    //    Intent intent = new Intent(this, StationActivity.class);
+    //    startActivity(intent);
+    //}
 }
