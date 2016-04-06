f729c11e23ac629686b26584bdf68feb212d461a
wefbak <rui.g.ribeiro@ist.utl.pt>
2016-04-06 18:51:04 +0100

    Screens added except login one

---
+++ UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/FriendProfile.java
@@ -0,0 +1,26 @@
+package pt.ulisboa.tecnico.cmov.ubibike;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FriendProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_profile);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MessageActivity.class);
+        startActivity(intent);
+    }
+
+    public void givePoints(View view) {
+        Intent intent = new Intent(this, GivePointsActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/GivePointsActivity.java
@@ -0,0 +1,15 @@
+package pt.ulisboa.tecnico.cmov.ubibike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class GivePointsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_give_points);
+    }
+}
