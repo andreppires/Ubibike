e8215991e050370edafb85691f2322f9c1642e4f
André Pires <andre.pinheiro.pires@gmail.com>
2016-04-12 16:38:22 +0100

    bug fixed on layout. Server exits. Run Server to run app

---
+++ UbiBike/Server.java
@@ -0,0 +1,21 @@
+import java.net.*;
+import java.io.*;
+
+public class Server{
+
+
+    public static void main(String args[]) throws IOException {
+
+        ServerSocket servidor = new ServerSocket(12345);
+        System.out.println("Porta 12345 aberta!");
+
+        Socket cliente = servidor.accept();
+        System.out.println("Nova conexão com o cliente "+cliente.getInetAddress().getHostAddress());
+        // imprime o ip do cliente
+
+
+
+    }
+
+
+}
