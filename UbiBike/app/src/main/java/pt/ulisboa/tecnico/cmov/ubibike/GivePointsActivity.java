556817d797622e85158bfd11a085ce858e522e2b
marianamv112 <marianamv112@gmail.com>
2016-04-13 11:38:28 +0100

    tou a tentar fazer a ligaçao das cenas e mudei nomes as coisas

--- UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/GivePointsActivity.java
+++ UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/GivePointsActivity.java
@@ -10,6 +10,7 @@ public class GivePointsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_give_points);
     }
 }
