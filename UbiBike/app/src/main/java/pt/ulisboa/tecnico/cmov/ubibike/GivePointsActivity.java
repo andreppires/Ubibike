f6f7c767b0072cc693a4450f5c37c25f535a8294
marianamv112 <marianamv112@gmail.com>
2016-04-14 15:27:31 +0100

    GivePointsActivity a bombar bué a nível funcional

--- UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/GivePointsActivity.java
+++ UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/GivePointsActivity.java
@@ -1,9 +1,17 @@
 package pt.ulisboa.tecnico.cmov.ubibike;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class GivePointsActivity extends AppCompatActivity {
 
@@ -12,5 +20,37 @@ public class GivePointsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_give_points);
+
+
+        final EditText et = (EditText) findViewById(R.id.editText);
+        final TextView tx = (TextView) findViewById(R.id.textView7);
+        final TextView tx8 = (TextView) findViewById(R.id.textView8);
+        Button b = (Button)findViewById(R.id.button);
+
+        et.setInputType(InputType.TYPE_CLASS_NUMBER);
+        tx8.setInputType(InputType.TYPE_CLASS_NUMBER);
+        tx.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final int friendsPoints = Integer.parseInt(tx.getText().toString());
+                final int pointsToSend = Integer.parseInt(et.getText().toString());
+                final int myPoints = Integer.parseInt(tx8.getText().toString());
+
+                if(myPoints > 0) {
+                    tx.setText(Integer.toString(friendsPoints + pointsToSend));
+                    tx8.setText(Integer.toString(myPoints - pointsToSend));
+                } else {
+
+                    Toast.makeText(v.getContext(), "Não existem pontos para enviar", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
+
+
+
 }
