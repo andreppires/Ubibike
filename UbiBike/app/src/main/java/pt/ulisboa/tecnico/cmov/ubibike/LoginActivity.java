b3efd572d60ab03ea61ff05ea1651c9609e14a83
André Pires <andre.pinheiro.pires@gmail.com>
2016-05-11 15:02:59 +0100

    closes #2

--- UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/LoginActivity.java
+++ UbiBike/app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/LoginActivity.java
@@ -182,7 +182,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
-            homeActivity();
             finish();
         }
     }
